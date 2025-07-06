8318b68c27a7952814f943c19e98e9d4b2745afa
Riya Gattellu <riya.gattellu@impressico.com>
2025-07-06 15:36:56 +0530

    ts error

--- src/components/AI-Settings/Configuration/Configuration.tsx
+++ src/components/AI-Settings/Configuration/Configuration.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import {
   SettingsContainer,
