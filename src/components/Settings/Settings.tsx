116466a011493f6e99a359c982da639f7f2849a2
Anjali Sahu <anjali.s@goldeneagle.ai>
2025-07-08 12:36:49 +0530

    integrate TOTP-based 2FA UI with QR scan and OTP verification

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -2,9 +2,13 @@ import { useState } from "react";
 import { SettingsContainer } from "./settings.styled";
 import SettingsList from "./SettingsList/SettingsList";
 import NotificationSettings from "./NotificationSettings/NotificationSettings";
+import SecuritySettings from "../Settings/SecuritySetting/securitySetting";
+import Cookies from "js-cookie"; 
 
 const Settings = () => {
   const [selectedTab, setSelectedTab] = useState<string>("Notifications");
+
+  const token = Cookies.get("access_token") || "";
 
   return (
     <SettingsContainer>
@@ -12,7 +16,9 @@ const Settings = () => {
         selectedTab={selectedTab}
         setSelectedTab={setSelectedTab}
       />
-      {selectedTab === "Notifications" && (<NotificationSettings />)}
+
+      {selectedTab === "Notifications" && <NotificationSettings />}
+      {selectedTab === "Security" && <SecuritySettings token={token} />}
     </SettingsContainer>
   );
 };
