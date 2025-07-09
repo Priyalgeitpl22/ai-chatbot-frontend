ec098f20ce538e1830a897594c09b8fbef8b9ee7
Riya Gattellu <riya.gattellu@impressico.com>
2025-07-09 12:04:26 +0530

    Update AppSubtitle text in Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -91,7 +91,7 @@ const Header = () => {
         <img src={logo} style={{ width: "55px", height: "55px", cursor:'pointer' }} onClick={() => navigate("/")}></img>
         <TitleContainer>
           <AppTitle>Golden Bot</AppTitle>
-          <AppSubtitle>Automate, Assist, Accelerate</AppSubtitle>
+          <AppSubtitle>Automate, Assist, Accelerateeeee</AppSubtitle>
         </TitleContainer>
       </LogoContainer>
       <HeaderOptions>
